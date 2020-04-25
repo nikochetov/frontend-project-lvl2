a39ed296ebff81a363465e8193e246908ab508f1
Nikolay Kochetov <nikochetov@gmail.com>
2020-04-25 20:21:43 +0300

    update gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,5 +4,7 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstconfig> <secondConfig>')
   .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
