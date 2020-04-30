40548ec8b505201777b8c5e732152b6ae134ea95
Nikolay Kochetov <nikochetov@gmail.com>
2020-04-30 21:11:52 +0300

    add test for yaml files

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -2,10 +2,11 @@ import genDiff from '../src/index.js';
 import path from 'path';
 import fs from 'fs';
 
-test('should work', () => {
-  const afterPath = path.join(__dirname, '..', '__fixtures__', 'after.json');
-  const beforePath = path.join(__dirname, '..', '__fixtures__', 'before.json');
-  const expected = `{
+describe('testing diff', () => {
+  test('testing gendiff for JSON files', () => {
+    const afterPath = path.join(__dirname, '..', '__fixtures__', 'after.json');
+    const beforePath = path.join(__dirname, '..', '__fixtures__', 'before.json');
+    const expected = `{
     host: hexlet.io
   + timeout: 20
   - timeout: 50
@@ -14,4 +15,18 @@ test('should work', () => {
   + verbose: true
 }`
   expect(genDiff(beforePath, afterPath)).toEqual(expected);
+  });
+  test('testing gendiff for YAML files', () => {
+    const afterPath = path.join(__dirname, '..', '__fixtures__', 'after.yml');
+    const beforePath = path.join(__dirname, '..', '__fixtures__', 'before.yml');
+    const expected = `{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  - follow: false
+  + verbose: true
+}`
+  expect(genDiff(beforePath, afterPath)).toEqual(expected);
+  });
 });
