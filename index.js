a3277a9612dd7cb89a0371f050c42668a051285d
Nikolay Kochetov <nikochetov@gmail.com>
2020-06-11 12:26:36 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import genDiff from './src/genDiff.js';
+import parser from './src/parsers.js';
+import render from './src/formatters/index.js';
 
-export default genDiff;
+export default (path1, path2, format) => {
+  const [beforeFileData, afterFileData] = parser(path1, path2);
+  const diff = genDiff(beforeFileData, afterFileData);
+  const rendering = render(format);
+  return rendering(diff);
+};
