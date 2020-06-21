7e40c1d43290f0d111c3e6cfba87edf28ba7be73
Nikolay Kochetov <nikochetov@gmail.com>
2020-06-21 23:16:32 +0300

    remove reading files from parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,17 @@
-import fs from 'fs';
+// import fs from 'fs';
 import yaml from 'js-yaml';
 import ini from 'ini';
 import path from 'path';
 
-export default (path1, path2) => {
-  const formatOfPath1 = path.extname(path1);
-  const formatOfPath2 = path.extname(path2);
+export default (pathToFile, data) => {
+  const format = path.extname(pathToFile);
   let parse;
-  if (formatOfPath1 && formatOfPath2 === '.json') {
+  if (format === '.json') {
     parse = JSON.parse;
-  } else if (formatOfPath1 && formatOfPath2 === '.yml') {
+  } else if (format === '.yml') {
     parse = yaml.safeLoad;
-  } else if (formatOfPath1 && formatOfPath2 === '.ini') {
+  } else if (format === '.ini') {
     parse = ini.parse;
   }
-  const firstFileData = parse(fs.readFileSync(path1, 'utf-8'));
-  const secondFileData = parse(fs.readFileSync(path2, 'utf-8'));
-  return [firstFileData, secondFileData];
+  return parse(data);
 };
