d017781397f17e8915325d07c1b2b3c502fde07f
Nikolay Kochetov <nikochetov@gmail.com>
2020-06-21 23:16:47 +0300

    update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
+
 const stringify = (value) => {
-  if (typeof value === 'object') {
+  if (_.isObject(value)) {
     return '[complex value]';
   }
   return `'${value}'`;
@@ -7,21 +9,22 @@ const stringify = (value) => {
 
 export default (diff) => {
   const iter = (idiff, path) => {
-    const rendering = idiff.reduce((prev, current) => {
-      if (current.type === 'node') {
-        return [...prev, iter(current.children, `${path}${current.name}.`)];
-      }
-      switch (current.status) {
-        case 'modified':
-          return [...prev, `Property '${path}${current.name}' was changed from ${stringify(current.before)} to ${stringify(current.after)}`];
-        case 'removed':
-          return [...prev, `Property '${path}${current.name}' was deleted`];
-        case 'added':
-          return [...prev, `Property '${path}${current.name}' was added with value: ${stringify(current.value)}`];
-        default:
-          return [...prev];
-      }
-    }, []);
+    const rendering = idiff
+      .filter((node) => node.status !== 'unchanged')
+      .map((node) => {
+        switch (node.status) {
+          case 'modified':
+            return `Property '${path}${node.name}' was changed from ${stringify(node.before)} to ${stringify(node.after)}`;
+          case 'removed':
+            return `Property '${path}${node.name}' was deleted`;
+          case 'added':
+            return `Property '${path}${node.name}' was added with value: ${stringify(node.value)}`;
+          case 'parent':
+            return iter(node.children, `${path}${node.name}.`);
+          default:
+            throw new Error(`Error! '${node.status}' is invalid`);
+        }
+      }, []);
     return rendering.join('\n');
   };
   return iter(diff, '');
