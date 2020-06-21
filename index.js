ea09e8f4b6e2284dd820d61929b6510fd7e4b71d
Nikolay Kochetov <nikochetov@gmail.com>
2020-06-21 23:33:51 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import path from 'path';
 import fs from 'fs';
 import genDiff from './src/genDiff.js';
-import toParse from './src/parsers.js';
-import render from './src/formatters/index.js';
+import getParser from './src/parsers.js';
+import getRender from './src/formatters/index.js';
 
 const readFile = (pathToFile) => {
   const fullPath = path.resolve(process.cwd(), pathToFile);
   const fileData = fs.readFileSync(fullPath, 'utf-8');
   return fileData;
 };
-const parsedFileData = (pathToFile) => toParse(pathToFile, readFile(pathToFile));
+const makeParsing = (pathToFile) => getParser(pathToFile, readFile(pathToFile));
 
 export default (path1, path2, format) => {
-  const beforeFileData = parsedFileData(path1);
-  const afterFileData = parsedFileData(path2);
+  const beforeFileData = makeParsing(path1);
+  const afterFileData = makeParsing(path2);
   const diff = genDiff(beforeFileData, afterFileData);
-  const doRender = render(format);
-  return doRender(diff);
+  const makeRender = getRender(format);
+  return makeRender(diff);
 };
