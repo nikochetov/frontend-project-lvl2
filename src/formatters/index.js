25745b1eed23d6bfcb2883289671c2c5a309e62c
Nikolay Kochetov <nikochetov@gmail.com>
2020-06-25 21:52:55 +0300

    update index.js in formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,13 +3,14 @@ import plain from './plain.js';
 import json from './json.js';
 
 export default (format) => {
-  let render;
-  if (format === 'json') {
-    render = json;
-  } else if (format === 'stylish') {
-    render = stylish;
-  } else if (format === 'plain') {
-    render = plain;
+  switch (format) {
+    case 'json':
+      return json;
+    case 'stylish':
+      return stylish;
+    case 'plain':
+      return plain;
+    default:
+      throw new Error(`Invalid format: ${format}`);
   }
-  return render;
 };
